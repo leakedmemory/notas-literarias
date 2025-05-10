ef45f1ff027b53a3d02db68e74ef2c80d82a892c
leaked memory <leaked.memory@pm.me>
2025-05-10 20:30:36 -0300

    refactor(logger): cria função para pegar o tempo atual já formatado

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -3,31 +3,30 @@ import dayjs from "dayjs";
 const PREFIX = "NOTAS LITERARIAS";
 const DATETIME_FORMAT = "HH:mm:ss";
 
+function now(): string {
+  return dayjs().format(DATETIME_FORMAT);
+}
+
 const logger = {
   /** Logs `msg` with the extension's prefix. */
   // biome-ignore lint: accept explicit `any` in this case
   log(msg: any) {
-    const now = dayjs().format(DATETIME_FORMAT);
-    console.log(`[${now}] ${PREFIX}: ${msg}`);
+    console.log(`[${now()}] ${PREFIX}: ${msg}`);
   },
 
   /** Logs `msg` with the extension's prefix at "error" level. */
   // biome-ignore lint: accept explicit `any` in this case
   error(msg: any) {
-    const now = dayjs().format(DATETIME_FORMAT);
-    console.error(`[${now}] ${PREFIX}: ${msg}`);
+    console.error(`[${now()}] ${PREFIX}: ${msg}`);
   },
 
   /**
-   * Logs `obj`, identified by `name`, in JSON format and with
-   * the extension's prefix.
-   *
-   * Should only be used for debugging purposes.
+   * Logs `obj`, identified by `name` and at debug level, in JSON format and
+   * with the extension's prefix.
    */
   pretty(obj: object, name?: string) {
-    const now = dayjs().format(DATETIME_FORMAT);
     console.debug(
-      `[${now}] ${PREFIX}: ${name ?? "obj"} = ${JSON.stringify(obj, null, 2)}`,
+      `[${now()}] ${PREFIX}: ${name ?? "obj"} = ${JSON.stringify(obj, null, 2)}`,
     );
   },
 };
