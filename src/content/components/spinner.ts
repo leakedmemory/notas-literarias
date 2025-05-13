69c4a349a48dd42d43d9630793117204d5d291f3
leaked memory <leaked.memory@pm.me>
2025-05-13 17:31:25 -0300

    refactor: move css do spinner para arquivo próprio

--- src/content/components/spinner.ts
+++ src/content/components/spinner.ts
@@ -1,9 +1,10 @@
 import logger from "../../shared/logger";
 
 import { getRatingReference } from "./rating";
+import spinnerCSS from "./spinner.css?raw";
 
 /**
- * Inserts the loading spinner into the page
+ * Inserts loading spinner into page.
  */
 export function insertLoadingSpinner() {
   const ratingRef = getRatingReference();
@@ -34,7 +35,7 @@ export function insertLoadingSpinner() {
 }
 
 /**
- * Removes the loading spinner from the page
+ * Removes loading spinner from page.
  */
 export function removeLoadingSpinner() {
   const spinnerContainer = document.getElementById(
@@ -48,29 +49,12 @@ export function removeLoadingSpinner() {
 }
 
 /**
- * Inserts the CSS for the spinner
+ * Inserts the CSS for spinner.
  */
 export function insertSpinnerStyles() {
-  const css = `
-    .notasliterarias-loader {
-      width: 24px;
-      aspect-ratio: 1;
-      border-radius: 50%;
-      background: 
-        radial-gradient(farthest-side,#ffa516 94%,#0000) top/8px 8px no-repeat,
-        conic-gradient(#0000 30%,#ffa516);
-      -webkit-mask: radial-gradient(farthest-side,#0000 calc(100% - 8px),#000 0);
-      animation: notasliterarias-spinner 1s infinite linear;
-    }
-    
-    @keyframes notasliterarias-spinner {
-      100% { transform: rotate(1turn) }
-    }
-  `;
-
   const styleElement = document.createElement("style");
   styleElement.type = "text/css";
-  styleElement.appendChild(document.createTextNode(css));
+  styleElement.appendChild(document.createTextNode(spinnerCSS));
 
   logger.log("inserting spinner styles");
 
