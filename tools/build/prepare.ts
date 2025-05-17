2368aa8e393d15627602334e565aa6423901f600
leaked memory <leaked.memory@pm.me>
2025-05-17 15:40:44 -0300

    feat: cria ícone para extensão

--- tools/build/prepare.ts
+++ tools/build/prepare.ts
@@ -1,33 +1,52 @@
-import { copyFile as fsCopyFile, mkdir } from "node:fs/promises";
+import { copyFile, mkdir, cp } from "node:fs/promises";
 import { existsSync } from "node:fs";
 import { dirname } from "node:path";
 
 import { res, isDev, target } from "./utils";
 
+const outDir = res("dist", isDev ? "dev" : "prod", target);
+
 (function main() {
   copyManifest();
+  copyIcons();
 })();
 
 async function copyManifest() {
   try {
-    const outDir = `dist/${isDev ? "dev" : "prod"}/${target}`;
-    const manifestSrc = res("manifests", `${target}.json`);
-    const manifestDest = res(outDir, "manifest.json");
+    const src = res("manifests", `${target}.json`);
+    const dest = res(outDir, "manifest.json");
 
-    if (!existsSync(manifestSrc)) {
-      console.error(`manifest not found for ${target}: ${manifestSrc}`);
+    if (!existsSync(src)) {
+      console.error(`manifest not found for ${target}: ${src}`);
       return;
     }
 
-    const destDir = dirname(manifestDest);
+    const destDir = dirname(dest);
     if (!existsSync(destDir)) {
       await mkdir(destDir, { recursive: true });
       console.log(`created directory: ${destDir}`);
     }
 
-    await fsCopyFile(manifestSrc, manifestDest);
+    await copyFile(src, dest);
     console.log(`successfully copied manifest for ${target}`);
   } catch (error) {
     console.error(`error copying manifest for ${target}: ${error}`);
+  }
+}
+
+async function copyIcons() {
+  try {
+    const src = res("icons");
+    const dest = res(outDir, "icons");
+
+    if (!existsSync(outDir)) {
+      await mkdir(outDir, { recursive: true });
+      console.log(`created directory: ${outDir}`);
+    }
+
+    await cp(src, dest, { recursive: true });
+    console.log(`successfully copied icons directory to ${dest}`);
+  } catch (error) {
+    console.error(`error copying icons directory: ${error}`);
   }
 }
